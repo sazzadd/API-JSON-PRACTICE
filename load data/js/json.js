3fb95823169e9a3b3b8b9325c834977d51b886bd
sazzad <sazzadador@gmail.com>
2024-10-01 11:41:14 +0600

    fundamental parctice API

---
+++ load data/js/json.js
@@ -0,0 +1,5 @@
+const user ={id:1, name:'sazzad', job: 'actor'};
+// const stringify = JSON.stringify(user);
+console.log(user);
+console.log(JSON.stringify(user));
+console.log(JSON.parse(JSON.stringify(user)))
